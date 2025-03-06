e60b33738ca6b7b248a55c4cfc2a98e50aac36a7
MaximeFREY <maximefrey41@gmail.com>
2025-03-06 19:12:45 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 // config.php
 $host = 'localhost';
 $dbname = 'api_database';
-$username = 'lrutella';      // modifiez selon votre configuration
+$username = 'root';      // modifiez selon votre configuration
 $password = 'Espoir15!';          // modifiez selon votre configuration
